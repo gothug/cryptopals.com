a135b1538f9105cc8a422ae44e509e7f6981f73f
gothug <kojuhovskiy@gmail.com>
2014-08-30 23:54:22 +0400

    fixed identation in challenge-2, scala

--- scala/set1/challenge-2/src/main/scala/Ex2.scala
+++ scala/set1/challenge-2/src/main/scala/Ex2.scala
@@ -1,13 +1,13 @@
 package ex2
 
 object Ex2 {
-	def xor(hexStr1: String, hexStr2: String) = {
-		List(hexStr1, hexStr2).map(_.grouped(1).toList.
-			map(Integer.parseInt(_, 16)))
-				match {
-					case x1::x2::Nil => (x1 zip x2).map(tuple => tuple._1 ^ tuple._2).
-																map(_.toHexString).mkString
-					case _					 => throw new IllegalArgumentException("Bad params")
-				}
-	}
+  def xor(hexStr1: String, hexStr2: String) = {
+    List(hexStr1, hexStr2).map(_.grouped(1).toList.
+      map(Integer.parseInt(_, 16)))
+        match {
+          case x1::x2::Nil => (x1 zip x2).map(tuple => tuple._1 ^ tuple._2).
+                                map(_.toHexString).mkString
+          case _           => throw new IllegalArgumentException("Bad params")
+        }
+  }
 }
