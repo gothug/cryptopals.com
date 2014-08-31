015503bb17575ce5993789542fba464d6ede1264
gothug <kojuhovskiy@gmail.com>
2014-08-31 22:03:53 +0400

    challenge-4, scala

---
+++ scala/set1/challenge-4/build.sbt
@@ -0,0 +1,9 @@
+name := "Crypto challenges, set 1, problem 1"
+
+version := "1.0"
+
+scalaVersion := "2.10.2"
+
+libraryDependencies += "org.scalatest" %% "scalatest" % "1.9.1" % "test"
+
+libraryDependencies += "junit" % "junit" % "4.10" % "test"
---
+++ scala/set1/challenge-4/src/main/scala/Challenge4.scala
@@ -0,0 +1,40 @@
+package challenge4
+
+object challenge4 {
+  def scoreCharacter(c: Char) =
+    if (c >= 'a' && c <= 'z' || c == ' ') 1
+    else if (c >= 'A' && c <= 'Z') 0.3
+    else if (c >= '0' && c <= '9') 0.2
+    else if (c == ',' || c == ''') 0.1
+    else 0
+
+  def scoreCharText(txt: List[Char]) = txt.map(scoreCharacter).reduce(_ + _)
+
+  def getOneByteXorTries(encStr: String) =
+      (0 to 255).map {
+        byte => {
+          val charTxt =
+            encStr.grouped(2).toList.map(Integer.parseInt(_, 16)).
+              map(_ ^ byte).map(_.toChar)
+
+          (byte, byte.toChar, scoreCharText(charTxt), charTxt.mkString,
+            encStr)
+        }
+      }
+
+  def main(args: Array[String]) {
+      val encStr =
+        "1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736"
+
+      val source = scala.io.Source.fromFile("4.txt")
+      val lines = source.mkString
+      source.close()
+
+      val tuple = lines.split('\n').flatMap(getOneByteXorTries).
+        sortWith(_._3 > _._3).head
+
+      println("Encoded text: " + tuple._4)
+      println("Encoded string: " + tuple._5)
+      println("Encoding character: " + tuple._2)
+  }
+}
